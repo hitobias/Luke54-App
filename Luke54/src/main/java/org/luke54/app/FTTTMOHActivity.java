7eada19a172f950947d9838d942bda02e0549cc3
hitobias <hitobias@gmail.com>
2013-09-17 13:45:33 +0800

    1.2.2.1
    1. change image load function
    2. bug fix

--- Luke54/src/main/java/org/luke54/app/FTTTMOHActivity.java
+++ Luke54/src/main/java/org/luke54/app/FTTTMOHActivity.java
@@ -12,51 +12,14 @@ import android.widget.ImageView;
 
 public class FTTTMOHActivity extends Activity {
 
-    private  int[] imagesId = {
-            R.drawable.fttt_moh1,
-            R.drawable.fttt_moh2,
-            R.drawable.fttt_moh3,
-            R.drawable.fttt_moh4,
-            R.drawable.fttt_moh5,
-            R.drawable.fttt_moh6,
-            R.drawable.fttt_moh7,
-            R.drawable.fttt_moh8
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        RealViewSwitcher realViewSwitcher = new RealViewSwitcher(getApplicationContext());
-
-
-
-        for (int i = 0; i < imagesId.length; i++) {
-            ImageView iv = new ImageView(this);
-            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            iv.setLayoutParams(lp);
-            iv.setBackgroundResource(imagesId[i]);
-            realViewSwitcher.addView(iv);
-        }
-        setContentView(realViewSwitcher);
-
-        realViewSwitcher.setOnScreenSwitchListener(onScreenSwitchListener);
+        setContentView(R.layout.fttt_moh);
     }
-
-        private final RealViewSwitcher.OnScreenSwitchListener onScreenSwitchListener =
-                new RealViewSwitcher.OnScreenSwitchListener() {
-                    @Override
-                    public void onScreenSwitched(final int screen) {
-                    /*
-                     * this method is executed if a screen has been activated, i.e. the screen is
-                     * completely visible and the animation has stopped (might be useful for
-                     * removing / adding new views)
-                     */
-                        Log.d("HorizontalPager", "switched to screen: " + screen);
-                    }
-                };
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Luke54/src/main/java/org/luke54/app/MinistryOfLifeActivity.java
+++ Luke54/src/main/java/org/luke54/app/MinistryOfLifeActivity.java
@@ -15,64 +15,12 @@ import java.util.List;
 
 public class MinistryOfLifeActivity extends Activity {
 
-    private  int[] imagesId = {
-            R.drawable.mohl1,
-            R.drawable.mohl2,
-            R.drawable.mohl3,
-            R.drawable.mohl4,
-            R.drawable.mohl5,
-            R.drawable.mohl6,
-            R.drawable.mohl7,
-            R.drawable.mohl8,
-            R.drawable.mohl9,
-            R.drawable.mohl10,
-            R.drawable.mohl11,
-            R.drawable.mohl12,
-            R.drawable.mohl13,
-            R.drawable.mohl14,
-            R.drawable.mohl15,
-            R.drawable.mohl16,
-            R.drawable.mohl17,
-            R.drawable.mohl18,
-            R.drawable.mohl19,
-            R.drawable.mohl20,
-            R.drawable.mohl21
-    };
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         getActionBar().setDisplayHomeAsUpEnabled(true);
-        RealViewSwitcher realViewSwitcher = new RealViewSwitcher(getApplicationContext());
-
-
-
-        for (int i = 0; i < imagesId.length; i++) {
-            ImageView iv = new ImageView(this);
-            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            iv.setLayoutParams(lp);
-            iv.setBackgroundResource(imagesId[i]);
-            realViewSwitcher.addView(iv);
-        }
-        setContentView(realViewSwitcher);
-
-        realViewSwitcher.setOnScreenSwitchListener(onScreenSwitchListener);
+        setContentView(R.layout.ministry_of_life);
     }
-
-    private final RealViewSwitcher.OnScreenSwitchListener onScreenSwitchListener =
-            new RealViewSwitcher.OnScreenSwitchListener() {
-                @Override
-                public void onScreenSwitched(final int screen) {
-                    /*
-                     * this method is executed if a screen has been activated, i.e. the screen is
-                     * completely visible and the animation has stopped (might be useful for
-                     * removing / adding new views)
-                     */
-                    Log.d("HorizontalPager", "switched to screen: " + screen);
-                }
-            };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
